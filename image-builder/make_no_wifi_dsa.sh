a9ed5e57772ab23a219daa024f1f15bbb759a377
Your Name <you@example.com>
2026-01-10 04:23:31 +0800

    use imagebuilder
    set oaf default m
    
    move mkimage dir
    
    test llvm clang version
    
    test version
    
    test version
    
    modify cache key
    
    add  clang llvm
    
    modify runtime
    
    fix mkimage

---
+++ image-builder/make_no_wifi_dsa.sh
@@ -0,0 +1,12 @@
+make image PROFILE=nsy_g68-plus PACKAGES="-kmod-switch-rtl8367b -swconfig \
+kmod-dsa kmod-phy-realtek kmod-dsa-realtek kmod-dsa-rtl8365mb \
+kmod-button-hotplug kmod-leds-gpio \
+luci luci-ssl luci-compat luci-i18n-base-zh-cn \
+luci-i18n-firewall-zh-cn luci-app-samba4 luci-i18n-samba4-zh-cn luci-app-upnp \
+luci-i18n-upnp-zh-cn luci-app-https-dns-proxy luci-i18n-https-dns-proxy-zh-cn \
+luci-app-adblock luci-i18n-adblock-zh-cn luci-app-wol luci-i18n-wol-zh-cn kmod-tun \
+zerotier tcpdump block-mount ca-certificates \
+kmod-oaf luci-app-oaf luci-i18n-oaf-zh-cn openssh-sftp-server -libustream-mbedtls libustream-openssl20201210 -apk-mbedtls apk-openssl \
+" \
+FILES=dsa \
+EXTRA_IMAGE_NAME=dsa_no_wifi
---
+++ image-builder/make_wifi_mt_sw.sh
@@ -0,0 +1,15 @@
+make image PROFILE=nsy_g68-plus PACKAGES="kmod-switch-rtl8367b swconfig \
+-kmod-dsa -kmod-phy-realtek -kmod-dsa-realtek -kmod-dsa-rtl8365mb \
+-kmod-cfg80211 -kmod-mac80211 -kmod-mt76-connac -kmod-mt76-core -kmod-mt7915e -kmod-mt7916-firmware  -wpad-mesh-openssl -wifi-scripts \
+iwinfo wireless-tools wireless-regdb \
+wpad-openssl luci-app-mtk  luci-i18n-mtk-zh-cn datconf datconf-lua kmod-mt_wifi wifi-dats wifi-dats wifi-profile \
+kmod-button-hotplug kmod-leds-gpio \
+luci luci-ssl luci-compat luci-i18n-base-zh-cn \
+luci-i18n-firewall-zh-cn luci-app-samba4 luci-i18n-samba4-zh-cn luci-app-upnp \
+luci-i18n-upnp-zh-cn luci-app-https-dns-proxy luci-i18n-https-dns-proxy-zh-cn \
+luci-app-adblock luci-i18n-adblock-zh-cn luci-app-wol luci-i18n-wol-zh-cn kmod-tun \
+zerotier tcpdump block-mount ca-certificates \
+kmod-oaf luci-app-oaf luci-i18n-oaf-zh-cn openssh-sftp-server -libustream-mbedtls libustream-openssl20201210 -apk-mbedtls apk-openssl \
+" \
+FILES=sw \
+EXTRA_IMAGE_NAME=sw_mt
