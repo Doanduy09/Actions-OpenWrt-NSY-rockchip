7498a85011b774c711724d46de0647af0d83adff
Your Name <you@example.com>
2026-01-20 17:45:23 +0800

    use immortal source
    restore release
    
    fix script
    
    fix script
    
    fix script

--- packages/compile_packages.sh
+++ packages/compile_packages.sh
@@ -3,14 +3,21 @@ mkdir custfeed
 sed -i "1i src-link external $dir/custfeed" feeds.conf.default
 
 #sed -i '1i src-git pppoex https://github.com/pppoex/openwrt-packages.git' feeds.conf.default
-git clone https://github.com/pppoex/openwrt-packages.git pppoex
-mv pppoex/luci-app-cpufreq custfeed/
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git custfeed/luci-app-ddns-go
-git clone https://github.com/QiDream-hub/luci-app-zerotier.git custfeed/luci-app-zerotier
+git clone https://github.com/immortalwrt/immortalwrt.git immortalwrt --depth=1
+git clone https://github.com/immortalwrt/luci.git  immortalwrt_luci  --depth=1
+git clone https://github.com/immortalwrt/packages.git immortalwrt_package  --depth=1
+cp -rp immortalwrt/package/emortal/cpufreq custfeed/
+cp -rp immortalwrt_package/net/ddns-go custfeed/
+cp -rp immortalwrt_luci/applications/luci-app-cpufreq custfeed/
+cp -rp immortalwrt_luci/applications/luci-app-ddns-go custfeed/
+cp -rp immortalwrt_luci/applications/luci-app-zerotier custfeed/
+
+find custfeed/ -type f -name 'Makefile' -exec sed -i 's|\.\./\.\./luci\.mk|$(TOPDIR)/feeds/luci/luci.mk|g' {} +
+find custfeed/ -type f -name 'Makefile' -exec sed -i 's|\.\./\.\./lang|$(TOPDIR)/feeds/packages/lang|g' {} +
 
 
 ./scripts/feeds update -a
-./scripts/feeds install -p external -a
+./scripts/feeds install -a
 #echo "# CONFIG_ALL_NONSHARED is not set" >> .config
 #echo "# CONFIG_ALL_KMODS is not set" >> .config
 #echo "# CONFIG_ALL is not set" >> .config
